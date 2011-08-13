10d3accbe518351b276ea4727439d0d96ec4ffca
Eugene Arshinov <earshinov@gmail.com>
2011-08-13 13:05:11 +0400

    gh-pages-examples: read fixed-header-js into demos/fixed-header-js/

---
+++ demos/fixed-header-js/localization.example.js
@@ -0,0 +1,21 @@
+var L10N = {
+
+  dict: {
+    //"Tags": "",
+    //"Hide": "",
+    //"Popular": "",
+    //"All": "",
+    //"Filter": "",
+    //"Choose from list": "",
+    //"Clear": ""
+  },
+
+  dictPlural: {
+    "note": ["note", "notes"]
+  },
+
+  // http://translate.sourceforge.net/wiki/l10n/pluralforms
+  plural: function(n){
+    return n != 1;
+  }
+};
